4f73a9363bd6f01daca50a9a5dcc3e79ab3616d3
Alexander Cleary <alexjacleary@gmail.com>
2024-01-03 12:54:12 +1100

    Fixed Header Ducking

--- assets/scripts/headerDucking.js
+++ assets/scripts/headerDucking.js
@@ -1,13 +1,33 @@
-let lastScrollTop = 0;
-const header = document.querySelector(".header");
+let header = document.querySelector(".header");
+document.addEventListener("DOMContentLoaded", function () {
+    let lastScrollTop = 0;
+    const scrollablePages = document.querySelectorAll(".parallax");
+    console.log(window.innerHeight)
 
-window.addEventListener("scroll", () => {
-    console.log("hi")
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop && scrollTop > window.innerHeight) {
-        header.style.top = "-120px";
-    } else {
-        header.style.top = "0";
-    }
-    lastScrollTop = scrollTop;
+    scrollablePages.forEach(page => {
+        page.addEventListener("scroll", () => {
+            if (!header) {
+                header = document.querySelector(".header");
+            }
+
+            let scrollTop = page.scrollTop;
+
+            if (scrollTop >= lastScrollTop && scrollTop > window.innerHeight) {
+                headerUp()
+            } else {
+                headerDown()
+            }
+            lastScrollTop = scrollTop;
+        });
+    });
 });
+function headerUp() {
+    header.style.height = "0";
+    header.style.opacity = "0";
+    header.style.pointerEvents = "none";
+}
+function headerDown() {
+    header.style.height = "120px";
+    header.style.opacity = "100";
+    header.style.pointerEvents = "all";
+}
